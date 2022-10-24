ace132b8005b450900acb6309030e0eabde9e151
Ryan Buchanan <buchanan.ryan22@gmail.com>
2022-10-24 13:40:42 -0600

    Add project files.

---
+++ A5-1_ASPNetCoreMVCProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace A5_1_ASPNetCoreMVCProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
